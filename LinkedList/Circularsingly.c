c34209ef991bc0482a472b5d82d9914d85182fb6
Himani Sharma <him.win@gmail.com>
2023-03-19 14:11:33 +0530

    Added programs for tree and graphs

--- LinkedList/Circularsingly.c
+++ LinkedList/Circularsingly.c
@@ -9,10 +9,10 @@ struct node *head;
   
 void beginsert ();   
 void lastinsert ();  
-void randominsert();  
+//void randominsert();  
 void begin_delete();  
 void last_delete();  
-void random_delete();  
+//void random_delete();  
 void display();  
 void search();  
 void main ()  
